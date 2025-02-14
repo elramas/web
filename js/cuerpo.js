e494a05359771624c0ea2bc2c47459294b8decfc
elramas <manelraga30@gmail.com>
2025-02-14 18:53:09 +0100

    Add files via upload

---
+++ js/cuerpo.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    const div = document.getElementById('t2');
+    div.style.top = '50%'; // Mueve el div al centro
+    div.style.transform = 'translateY(-50%)'; // Centra verticalmente
+};
---
+++ js/script.js
@@ -0,0 +1,6 @@
+//
+//window.onload = function() { 
+    //const imagen = document.getElementById('miImagen');
+    //imagen.style.top = '35%'; // Mueve la imagen al centro
+    //imagen.style.transform = 'translateY(-50%)'; // Centra verticalmente
+////};
