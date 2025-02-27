746eaecb06eede96007757cc315bb36ee5b92bdc
elramas <manelraga30@gmail.com>
2025-02-27 20:03:47 +0100

    cambos js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('mobile-menu'); // Seleccionar por ID
+    const menuToggle = document.getElementById('mobile-menu');
     const navList = document.querySelector('.nav-list');
 
+    if (!menuToggle) {
+        console.error("El elemento 'mobile-menu' no se encontró.");
+    }
+
+    if (!navList) {
+        console.error("El elemento 'nav-list' no se encontró.");
+    }
+
     menuToggle.addEventListener('click', function() {
-        navList.classList.toggle('active'); // Alternar la clase para mostrar/ocultar
+        navList.classList.toggle('active');
+        console.log("Menú clicado. Estado actual de 'active':", navList.classList.contains('active'));
     });
 });
+
